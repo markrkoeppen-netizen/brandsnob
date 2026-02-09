ef9869cc8f51d866c1b6254163508b24482a1e3f
markrkoeppen-netizen <mark.r.koeppen@gmail.com>
2026-02-09 15:25:28 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCTcD-ZlWn_ZLLYmbMLHc8n1kQFOlOqRM0",
+  authDomain: "brandsnobs-37142.firebaseapp.com",
+  projectId: "brandsnobs-37142",
+  storageBucket: "brandsnobs-37142.firebasestorage.app",
+  messagingSenderId: "871954882561",
+  appId: "1:871954882561:web:64458b4dab4fed8305e000",
+  measurementId: "G-YDR3W1YX0N"
 };
 
 // Initialize Firebase
