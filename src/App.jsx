515f902a714004b82dce3224e3fdfae81dfbc021
markrkoeppen-netizen <mark.r.koeppen@gmail.com>
2026-02-23 09:35:07 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -240,16 +240,16 @@ const RECOMMENDATIONS = [
 
 // All available brands for autocomplete
 const ALL_AVAILABLE_BRANDS = [
-  'Abercrombie & Fitch', 'Adidas', 'Allbirds', 'Alo', 'Ariat', 'Athleta', 'BIRKENSTOCK',
+  'Abercrombie & Fitch', 'Adidas', 'Allbirds', 'Alo', 'American Giant', 'Ariat', 'Athleta', 'BIRKENSTOCK',
   'Bombas', 'Brooks Brothers', 'Burberry', 'Burlebo', 'Carhartt', 'Chloé', 'Christian Louboutin',
   'Chubbies', 'Cinch', 'Coach', 'Cole Haan', 'Columbia', 'Costa', 'Crocs', 'Cruel Girl', 'Cult Gaia',
-  'Dacor', 'Dolce & Gabbana', 'Donna Karan', 'Estée Lauder', 'Fendi', 'Feragamo',
-  'Gucci', 'Havaianas', 'Hermès', 'Jimmy Choo', 'Justin Boots', 'Kate Spade', 'Kendra Scott', 'Kith', 'Lacoste',
+  'Dacor', 'Dolce & Gabbana', 'Donna Karan', 'Estée Lauder', 'Everlane', 'Fendi', 'Feragamo', 'Free People',
+  'Gorjana', 'Goyard', 'Gucci', 'Havaianas', 'Hermès', 'Jimmy Choo', 'Justin Boots', 'Kate Spade', 'Kendra Scott', 'Kith', 'Lacoste',
   'Levi Strauss', 'Louis Vuitton', 'Lucchese', 'Lululemon', 'Lush', 'Madewell', 'Marc Jacobs',
-  'Michael Kors', 'Nike', 'Oakley', 'OluKai', 'On Running', 'OOFOS', 'Oscar de la Renta', 'Panhandle Slim', 'Polo Ralph Lauren',
-  'Poncho Outdoors', 'Prada', 'Ray-Ban', 'Reef', 'Rhone', 'Saint Laurent', 'Sanuk', 'Stetson', 'Stuart Weitzman',
+  'Michael Kors', 'Nike', 'Oakley', 'OluKai', 'On Running', 'OOFOS', 'Oscar de la Renta', 'Panhandle Slim', 'Peter Millar', 'Polo Ralph Lauren',
+  'Poncho Outdoors', 'Prada', 'Ray-Ban', 'Reef', 'Rhone', 'Saint Laurent', 'Sanuk', 'Spanx', 'Stetson', 'Stuart Weitzman',
   'Teva', 'The North Face', 'The Row', 'Thom Browne', 'Tom Ford', 'Tommy Bahama', 'Tony Lama',
-  'Tory Burch', 'TravisMatthew', 'Tumi', 'UGG', 'Under Armour', 'Vera Wang',
+  'Tory Burch', 'TravisMatthew', 'Trendia', 'Tumi', 'UGG', 'Under Armour', 'Vera Wang',
   'Vineyard Vines', 'Vuori', 'Wrangler', 'Yeti', 'YoungLA'
 ];
 
